f068bd81e0f96c34e926fa8cf41ede1d90886817
Masaaki Tomii <masaaki.tomii1987@gmail.com>
2023-09-17 14:37:15 -0400

    Adding A2AVxPi

--- Hadrons/Modules.hpp
+++ Hadrons/Modules.hpp
@@ -8,6 +8,7 @@
 #include <Hadrons/Modules/MContraction/A2AFourQuarkContraction.hpp>
 #include <Hadrons/Modules/MContraction/A2ALoop.hpp>
 #include <Hadrons/Modules/MContraction/A2AMonsterLoop.hpp>
+#include <Hadrons/Modules/MContraction/A2AVxPi.hpp>
 #include <Hadrons/Modules/MContraction/A2ACoarseGrid.hpp>
 #include <Hadrons/Modules/MContraction/A2AMesonField.hpp>
 #include <Hadrons/Modules/MContraction/MultMesonField.hpp>
