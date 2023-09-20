9d28f045976e51cdb23ee4d7ac585c9f330c73e6
Masaaki Tomii <masaaki.tomii1987@gmail.com>
2023-09-20 12:26:35 -0400

    debugging

--- Hadrons/Modules/MContraction/A2APixW.hpp
+++ Hadrons/Modules/MContraction/A2APixW.hpp
@@ -188,8 +188,8 @@ void TA2APixW<FImpl>::execute(void)
 	  for(int s1=0;s1<Ns;s1++)
 	  for(int c1=0;c1<Nc;c1++){
 	    //vec0[sv]()(s1)(c1) += right()(s1)(c1) * meson[it](i,j);
-	    vec0[sv]()(s1)(c1) = right()(s1)(c1);
-	    //vec0[sv]()(s1)(c1) += Zero();
+	    vec0[sv]()(s1)(c1) =+ right()(s1)(c1);
+	    //vec0[sv]()(s1)(c1) = Zero();
 	  }
 	}}
       }
